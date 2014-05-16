70f4b113723bff00a5558ee0333e0b2ef2e07ac6
Jonas Bach <hallo@jonasbach.de>
2014-05-16 16:32:57 +0200

    add upload form

---
+++ assets/jbx_multiple_image_upload.js
@@ -0,0 +1,23 @@
+(function($, window, document, jbx_variables, undefined) {
+	if(typeof jbx_variables === 'undefined'){
+		return;
+	}
+	var markup = $('<form id="jbx_wrapper" action="index.php" method="post"><h2>' + jbx_variables.strings.upload_multiple + '</h2><p>' + jbx_variables.strings.cat_help + '</p><label>' + jbx_variables.strings.cat_title + ' <input type="text" id="jbx_category" name="jbx_category" /></label><div id="jbx_upload_wrapper"><input type="file" name="file_upload" id="jbx_upload" /></div><input name="event" value="'+ jbx_variables.slug +'" type="hidden" /><input name="step" value="import" type="hidden" /><button type="button" id="jbx_submit">'+ jbx_variables.strings.upload_button +'</button><h2>' + jbx_variables.strings.upload_single + '</h2></form>');
+	
+	$(function() {
+		var target = $('.upload-form');
+		if(target.length === 0){
+			return;
+		}
+
+		// insert markup
+		markup.insertBefore(target);
+
+		// init uploadify
+		$('#jbx_upload').uploadify({
+			'swf'		: jbx_variables.paths.swf,
+			'uploader'	: jbx_variables.paths.upload,
+			'auto'		: false
+		});
+	});
+})(jQuery, window, document, jbx_variables);
