7b501be1e8640a1705004921ce555ccc18667b93
Jonas Bach <hallo@jonasbach.de>
2014-05-18 20:35:17 +0200

    allow multiple upload fields

--- assets/jbx_multiple_image_upload.js
+++ assets/jbx_multiple_image_upload.js
@@ -2,19 +2,17 @@
 	if(typeof jbx_variables === 'undefined'){
 		return;
 	}
-	var markup = $('<div id="jbx_wrapper"><h2>' + jbx_variables.strings.upload_multiple + '</h2><p>' + jbx_variables.strings.cat_help + '</p><label>' + jbx_variables.strings.cat_title + ' <input type="text" id="jbx_category" name="jbx_category" /></label><div id="jbx_upload_wrapper"><input type="file" name="file_upload" id="jbx_upload" /></div><button type="button" id="jbx_submit" class="publish">'+ jbx_variables.strings.upload_button +'</button><h2>' + jbx_variables.strings.upload_single + '</h2></div>');
+	var markup = $('<div class="jbx_wrapper"><h2>' + jbx_variables.strings.upload_multiple + '</h2><p>' + jbx_variables.strings.cat_help + '</p><label>' + jbx_variables.strings.cat_title + ' <input type="text" class="jbx_category" name="jbx_category" /></label><div class="jbx_upload_wrapper"><input type="file" name="file_upload" class="jbx_upload" /></div><button type="button" class="jbx_submit publish">'+ jbx_variables.strings.upload_button +'</button><h2>' + jbx_variables.strings.upload_single + '</h2></div>');
 	
-	$(function() {
-		var $target = $('.upload-form');
-
-		if($target.length === 0){
-			return;
-		}
+	var initUploadify = function(){
+		var $target = $(this);
+		var $el = markup.clone();
 
 		// insert markup
-		markup.insertBefore($target);
-		var $submit = $('#jbx_submit');
-		var $upload = $('#jbx_upload');
+		$el.insertBefore($target);
+
+		var $submit = $el.find('.jbx_submit');
+		var $upload = $el.find('.jbx_upload').attr('id', 'jbx_upload_' + (Math.floor(Math.random() * 999999)));
 
 		// init uploadify
 		$upload.uploadify({
@@ -24,6 +22,7 @@
 			'buttonText': jbx_variables.strings.browse,
 			'fileSizeLimit': jbx_variables.fileSizeLimit,
 			'fileTypeDesc': jbx_variables.strings.images,
+			'fileTypeExts': '*.gif; *.jpg; *.png',
 			'buttonClass': 'navlink',
 			'onSelect' : function(file){
 				$submit.show();
@@ -35,5 +34,10 @@
 			e.preventDefault();
 			$upload.uploadify('upload');
 		});
+	};
+
+	// on domReady
+	$(function() {
+		$(jbx_variables.targets).each(initUploadify);
 	});
 })(jQuery, window, document, jbx_variables);
